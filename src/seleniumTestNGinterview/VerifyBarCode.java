fc459539de0c4411ae2bf160fb838a5a68a40802
sumithkv <sumith.kv@gmail.com>
2018-12-26 22:22:04 +0530

    BAR code

---
+++ src/seleniumTestNGinterview/VerifyBarCode.java
@@ -0,0 +1,12 @@
+package seleniumTestNGinterview;
+
+public class VerifyBarCode {
+
+	public static void main(String[] args) {
+	
+		// https://medium.com/@eliasnogueira/read-qr-code-content-with-selenium-and-zxing-59f255430fe0
+		
+		// https://advancedtestautomation.blogspot.com/2016/08/how-to-read-qr-code-or-barcode-from.html
+		
+	}
+}
--- src/seleniumTestNGinterview/VerifyCaptcha.java
+++ src/seleniumTestNGinterview/VerifyCaptcha.java
@@ -4,6 +4,9 @@ public class VerifyCaptcha {
 
 	public static void main(String[] args) {
 		
+		// https://www.quora.com/How-do-I-capture-a-captcha-in-an-automation-program-either-in-Watir-or-Selenium-Is-that-possible
+		
+		// https://sqa.stackexchange.com/questions/17022/how-to-fill-captcha-using-test-automation
 
 	}
 }
