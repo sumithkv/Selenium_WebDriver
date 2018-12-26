c6de40ccc9dd50db8b55d56a1d0ef059ca83cc15
sumithkv <sumith.kv@gmail.com>
2018-12-26 21:10:42 +0530

    javax email

--- src/seleniumTestNGinterview/VerifyEmailConfirmation.java
+++ src/seleniumTestNGinterview/VerifyEmailConfirmation.java
@@ -5,6 +5,10 @@ public class VerifyEmailConfirmation {
 	public static void main(String[] args) {
 	
 		// http://www.seleniumtests.com/2011/08/verify-email-confirmation-using.html
+		
+		// https://sqa.stackexchange.com/questions/17025/automation-testing-the-contents-in-email
 
+		//http://www.teachmeselenium.com/2016/09/27/how-to-automate-gmail-without-opening-it-in-browser-by-using-imap/
+		
 	}
 }
