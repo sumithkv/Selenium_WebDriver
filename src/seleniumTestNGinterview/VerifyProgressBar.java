23e4da38b4d655c91356db03d140e65de564303b
sumithkv <sumith.kv@gmail.com>
2018-12-26 22:44:17 +0530

    progress bar

--- src/seleniumTestNGinterview/VerifyProgressBar.java
+++ src/seleniumTestNGinterview/VerifyProgressBar.java
@@ -4,6 +4,7 @@ public class VerifyProgressBar {
 
 	public static void main(String[] args) {
 		
+		// https://developers.perfectomobile.com/display/TT/Speed+Up+Testing+-+Wait+For+Loading+Indicator+To+Disappear	
 
 	}
 }
