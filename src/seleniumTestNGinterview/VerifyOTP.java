2015d16ef3f87d6cdf59142ff3b589fe7976a4b5
sumithkv <sumith.kv@gmail.com>
2018-12-26 20:44:45 +0530

    OTPAutomation

--- src/seleniumTestNGinterview/VerifyOTP.java
+++ src/seleniumTestNGinterview/VerifyOTP.java
@@ -4,7 +4,11 @@ public class VerifyOTP {
 
 	public static void main(String[] args) {
 		
+		// Step 1: Connect the Phone/Dongle to the COM Port via USB.
+
+		// Step 2: Call the code for fetching sms via smslib.jar (https://stackoverflow.com/questions/36026904/how-to-automate-otp-using-selenium-web-driver)
+		
+		// Step 3: Parse sms to get the OTP from the fetched list by latest received sms
 
 	}
-
 }
