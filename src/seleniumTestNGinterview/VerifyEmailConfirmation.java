c72b9bc7d89a9ecbd1b7f24428e49649373f70e3
sumithkv <sumith.kv@gmail.com>
2018-12-26 21:00:35 +0530

    email & OTP

---
+++ src/seleniumTestNGinterview/VerifyEmailConfirmation.java
@@ -0,0 +1,10 @@
+package seleniumTestNGinterview;
+
+public class VerifyEmailConfirmation {
+
+	public static void main(String[] args) {
+	
+		// http://www.seleniumtests.com/2011/08/verify-email-confirmation-using.html
+
+	}
+}
--- src/seleniumTestNGinterview/VerifyOTP.java
+++ src/seleniumTestNGinterview/VerifyOTP.java
@@ -4,11 +4,9 @@ public class VerifyOTP {
 
 	public static void main(String[] args) {
 		
-		// Step 1: Connect the Phone/Dongle to the COM Port via USB.
+		// https://stackoverflow.com/questions/36026904/how-to-automate-otp-using-selenium-web-driver
 
-		// Step 2: Call the code for fetching sms via smslib.jar (https://stackoverflow.com/questions/36026904/how-to-automate-otp-using-selenium-web-driver)
+		// http://navyuginfo.com/automating-registration-otp-sms/
 		
-		// Step 3: Parse sms to get the OTP from the fetched list by latest received sms
-
 	}
 }
